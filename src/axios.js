d20f67515d81532be0c0be5728b2ab774fa0c27a
nadareffat98 <eng.nada.reffat@gmail.com>
2025-05-09 20:32:18 +0300

    new url

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 // import store from "./store/index.js";
 import i18n from "./i18n";
 
-axios.defaults.baseURL = process.env.VUE_APP_API_BASE + "/api/";
-// axios.defaults.baseURL='http://localhost/mobile/efatorh/api/';
+// axios.defaults.baseURL = process.env.VUE_APP_API_BASE + "/api/";
+axios.defaults.baseURL='https://dev.efatorh.com/api/';
 axios.defaults.headers.common["Authorization"] =
   "Bearer" + localStorage.getItem("token");
 axios.defaults.headers.common["X-localization"] = i18n.locale;
